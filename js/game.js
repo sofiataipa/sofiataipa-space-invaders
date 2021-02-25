4e720cf3a758a92bc2b8ed9fd66fa4e1b6af5117
Sofia <sofiataipa222@gmail.com>
2021-02-25 17:57:34 +0000

    sons

--- js/game.js
+++ js/game.js
@@ -6,12 +6,10 @@ let particles;
 let score;
 let currentLevel;
 let stars;
-// let starSpeed;
 
 // Projectile variables
 let projectileRadius = 4;
-let projectileColor = 'rgb(220, 159, 189)';  //'rgb(150, 40, 40)';
-// let projectileColor = 'rgb(200, 220, 220)';  //'rgb(150, 40, 40)';
+let projectileColor = 'rgb(220, 159, 189)';
 let projectileVelocity = 5;
 
 // Particle variables
@@ -21,11 +19,8 @@ let particleRadius = 2;
 let spawnEnemiesInterval;
 let spawnProjectilesInterval
 
-// Game constants
-const FRICTION = 0.99;
-
-// Animation TODO mudar nome
-let listener;
+// Animation Variables
+let playerListener;
 let numParticlesRatio = 2;
 
 // Game elements
@@ -37,6 +32,8 @@ let startGameBtn;
 let modalElement;
 let playerElement;
 
+// Game constants
+const FRICTION = 0.99;
 // Star Background
 const N_STARS = 80;
 const MAX_STAR_SIZE = 0.7;
@@ -44,10 +41,12 @@ const MIN_STAR_SIZE = 2;
 const STAR_INI_SPEED = 0.2;
 const STAR_COLOR = "rgb(255, 255, 255)";
 
+const laserSound = new Audio('../assets/laser.wav');
+const buttonClickSound = new Audio('../assets/buttonClick.wav');
+
 function main() { 
     // Game setup (Canvas)
     const cnv = document.getElementById('game');
-
     cnv.width = innerWidth;
     cnv.height = innerHeight;
 
@@ -58,16 +57,22 @@ function main() {
     lastScoreElement = $('#lastScore');
     startGameBtn = $('.btn')[0];
     modalElement = $('#modal');
-
     playerElement = $("#player");
-   
     playerElement.hide();
     statsElement.hide();
 
-    // EVENTS 
-    listener = function (event){
+    // Display the top 3 scores
+    displayScoreBoard();
+
+    /* 
+     * EVENTS 
+     */
+
+    // Add event that updates Player position 
+    playerListener = function (event){
         player.update(event, cnv);
     }
+
     // Init Game Button
     // Mobile
     startGameBtn.addEventListener('touchend', (event) => {
@@ -95,12 +100,13 @@ function main() {
 }
 
 function initGame(cnv) { 
+    buttonClickSound.play();
+    
     // Player variables
     let playerW = parseFloat(playerElement.css('width'));
     let playerH = parseFloat(playerElement.css('height'));
     let playerX = cnv.width/2;
     let playerY = cnv.height - (playerH);
-    // let playerColor = 'white';
 
     // Create player
     player = new Player(playerX, playerY, playerW);
@@ -110,54 +116,85 @@ function initGame(cnv) {
     enemies = [];
     particles  = [];
 
+    // Score and Level
     score = 0;
     currentLevel = 1;
-
-    spawnStars(cnv);
-
     levelElement.html(currentLevel);
-
     updateScore(scoreElement, 0);
     updateScore(lastScoreElement, 0);
 
+    // Create the star background
+    spawnStars(cnv);
+    
+    // Display Canvas and Stats bar
     cnv.style.display = "";
     statsElement.show();
 
-    animate();
-    
-    updateEnemyInterval();
-
+    // Hide Menu
     modalElement.removeClass('d-flex');
     modalElement.addClass('d-none');
+
+    // Add Player Movement event after 10 milisseconds
     setTimeout(() => {
-        window.addEventListener('mousemove', listener, false);
-        window.addEventListener('touchmove', listener, false);
+        window.addEventListener('mousemove', playerListener, false);
+        window.addEventListener('touchmove', playerListener, false);
     }, 10);
+
+    // Define enemy spawn interval in function of level
+    updateEnemyInterval();
+    // Define projectile spawn interval
     spawnProjectilesInterval = setInterval(spawnProjectiles, 300);
+
+    // Start animation
+    animate();
 }
 
 function endGame(cnv) {
+    // Stop enemy and projectile spawn
     clearInterval(spawnEnemiesInterval);
     clearInterval(spawnProjectilesInterval);
 
-    window.removeEventListener('click', listener, false);
-    window.removeEventListener('touchstart', listener, false);
+    // Remove Player Movement event
+    window.removeEventListener('click', playerListener, false);
+    window.removeEventListener('touchstart', playerListener, false);
+    
+    // Update the top 3 scores
+    displayScoreBoard();
 
+    // Show Menu
     modalElement.addClass('d-flex');
     modalElement.removeClass('d-none');
 
+    // Hide canvas, player and stats
     cnv.style.display = "none";
-
     playerElement.hide();
     statsElement.hide();
 }
 
-// Increase score
+/*
+ * Update and display the top 3 scores
+ */
+function displayScoreBoard() {
+    if(score > 0) {
+        saveScoreLS(score);
+    }
+    let topScores = getBestScoresLS(3);
+    for (let i = 0; i < topScores.length; i++) {
+        $(`#top${i+1}`).html(`${topScores[i]}`);
+    }
+}
+
+/*
+ * Increase score HTML
+ */
 function updateScore(scoreEl, value) {
     score += value;
     scoreEl.html(score);
 }
 
+/*
+ * Check if next level as begun in function of score
+ */
 function updateLevel() {
     let nextLevelScore = Math.round(2500 * Math.pow(currentLevel, 1.5));
     if(score > nextLevelScore) {
@@ -168,46 +205,46 @@ function updateLevel() {
     }
 }
 
+/*
+ * Interval of enemy spawn in function of level
+ */
 function updateEnemyInterval() {
     clearInterval(spawnEnemiesInterval);
     let interval = (1000*Math.pow(0.9, currentLevel));
     spawnEnemiesInterval = setInterval(spawnEnemies, interval > 500 ? interval : 500);
 }
 
-// Spawn Stars
-function spawnStars(cnv) {
-    
+/*
+ * Spawn Stars
+ */
+function spawnStars(cnv) {   
     stars = [];
     for (let i = 0; i < N_STARS; i++) {
-        // console.log("entrou")
         // Star variables
         let r = Math.random() * (MAX_STAR_SIZE - MIN_STAR_SIZE) + MIN_STAR_SIZE;
-        // let r = Math.random() * MAX_STAR_SIZE * cnv.height / 2;
         let x = Math.floor(Math.random() * cnv.width);
         let y = Math.floor(Math.random() * cnv.height);
         let v = Math.min((3*Math.pow(1.05, currentLevel)), 5) + r * 0.6;
-        // let v = STAR_INI_SPEED * cnv.height;
         
         stars.push(new Star(x, y, r, STAR_COLOR, v));
     }
 }
 
-// Update stars velocity (in function of level)
+/*
+ * Update stars velocity (in function of level)
+ */
 function updateStarsVelocity() {
     for (let i = 0; i < N_STARS; i++) {
         stars[i].setVelocity(Math.min((3*Math.pow(1.05, currentLevel)), 5) + stars[i].dim * 0.6);
     }
 }
 
-// Desenha as estrelas num canvas
-function drawStars(cnv) {
-   
-}
-
-// Spawn enemies
+/*
+ * Spawn enemies
+ */
 function spawnEnemies() {
     const cnv = document.querySelector('canvas');
-    let h = 0; //parseFloat(statsElement.css('height'));
+    let h = 0;
     let maxEnemyRadius = 50; 
     let EnemyRadius = Math.random() * (maxEnemyRadius - 10) + 10;
     let enemyX = Math.random() * (cnv.width - 2*maxEnemyRadius) + maxEnemyRadius; 
@@ -222,21 +259,28 @@ function spawnEnemies() {
     enemies.push(new Enemy(enemyX, enemyY, EnemyRadius, enemyColor, enemyVelocity, palavra));
 }
 
-// Spawn projectiles
+/*
+ * Spawn projectiles
+ */
 function spawnProjectiles() {
     projectiles.push(new Projectile(player.x, player.y -17, projectileRadius, projectileColor, projectileVelocity)); 
 }
 
-// Spawn particles
+/*
+ * Spawn particles
+ */
 function spawnParticles(posx, posy, color) {
     particles.push(new Particle(posx, posy, particleRadius, color));
 }
 
-// Animates frame by frame
+/*
+ * Animates frame by frame
+ */
 function animate() {
     const cnv = document.querySelector('canvas');
     const c = cnv.getContext('2d'); // Context
 
+    // Saves current frame
     animationId = requestAnimationFrame(animate);
     
     // Resizing frame
@@ -249,8 +293,10 @@ function animate() {
     c.fillStyle = 'rgb(20, 20, 20)'; 
     c.fillRect(0, 0, cnv.width, cnv.height);
     
+    // Check if next level as begun
     updateLevel();
 
+    // Draws player in current position of mouse or touch
     player.draw();
 
     // Draw and update stars
@@ -298,8 +344,7 @@ function animate() {
             updateScore(lastScoreElement, 0);
             modalElement.addClass('d-flex');
             modalElement.removeClass('d-none');
-            endGame(cnv);
-           
+            endGame(cnv);  
         }
 
         for(let j in projectiles) {
@@ -308,6 +353,7 @@ function animate() {
             
             // Collision detection (projectile and enemy)
             if(dist - (enemy.dim + projectile.dim) < 1) {
+                laserSound.play();
 
                 // Shrink if big enough
                 if(enemy.dim - 20 > 10 ) {
@@ -346,4 +392,38 @@ function animate() {
             endGame(cnv);          
         }
     }    
+}
+
+/*
+ * Saves last score on LocalStorage
+ */
+function saveScoreLS(score) {
+    let d = new Date();
+    let xmlTag = `<Score>${score}</Score>`
+    window.localStorage.setItem(d.getTime(), xmlTag);
+}
+
+/*
+ * Gets top scores on LocalStorage
+ */
+function getBestScoresLS(num) {
+    let top = [];
+
+    let indexStorage = window.localStorage.length;
+    for (let i = 0; i < indexStorage; i++) {
+        if(window.DOMParser){
+            let localStorageRow = window.localStorage.getItem(window.localStorage.key(i));
+            if(window.DOMParser) {
+                let parser = new DOMParser();
+                xmlDoc = parser.parseFromString(localStorageRow, "application/xml");
+            }
+        }
+
+        let scoreTag = xmlDoc.getElementsByTagName("Score");
+        top.push(scoreTag[0].textContent);
+    }
+    top.sort((a, b) => b - a);
+    if(top.length >= num)
+        top.slice(0, num);
+    return top;
 }
